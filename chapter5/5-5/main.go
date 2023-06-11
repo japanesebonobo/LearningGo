5e253e0e61ae4b66dbccf573e43c77ec38d77afa
japanesebonobo <tomi119119@gmail.com>
2023-06-12 00:32:28 +0900

    add 5-5

---
+++ chapter5/5-5/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type person struct {
+	age  int
+	name string
+}
+
+func modifyFails(i int, s string, p person) {
+	i *= 2
+	s = "さようなら"
+	p.name = "Bob"
+}
+
+func modMap(m map[int]string) {
+	m[2] = "こんにちは"
+	m[3] = "さようなら"
+	delete(m, 1)
+}
+
+func modSlice(s []int) {
+	for k, v := range s {
+		s[k] = v * 2
+	}
+	s = append(s, 10)
+}
+
+func main() {
+	m := map[int]string{
+		1: "1番目",
+		2: "2番目",
+	}
+	modMap(m)
+	fmt.Println(m)
+
+	s := []int{1, 2, 3}
+	modSlice(s)
+	fmt.Println(s)
+}
